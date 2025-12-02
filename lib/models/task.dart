5ca37f03382067437aceec191947d57bc3e43a3d
Felix-yaw <azaglowiliam@gmail.com>
2025-12-02 14:54:54 +0000

    Added sqlite integration to the app for persistence of data

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,11 +1,23 @@
 class Task {
-
+  int? id;
   String name;
   bool isDone;
 
-  Task({required this.name, this.isDone = false});
+  Task({this.id, required this.name, this.isDone = false});
 
-  void toggleDone(){
-    isDone = !isDone;
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'isDone': isDone ? 1 : 0,
+    };
+  }
+
+  factory Task.fromMap(Map<String, dynamic> map) {
+    return Task(
+      id: map['id'],
+      name: map['name'],
+      isDone: map['isDone'] == 1,
+    );
   }
 }
--- lib/provider/task_data.dart
+++ lib/provider/task_data.dart
@@ -1,37 +1,45 @@
 import 'package:flutter/foundation.dart';
+import 'package:todoey/helpers/db_helpers.dart';
 import 'package:todoey/models/task.dart';
 
 class TaskData extends ChangeNotifier {
+  List<Task> tasks = [];
 
-  List<Task> tasks = [
-    
-  ];
+  TaskData() {
+    loadTasksFromDB();
+  }
+
   int get taskCount => tasks.length;
-  
-  int get finishedTasks => tasks.where((task) => !task.isDone).length;
-  int get unfinishedTasks => tasks.where((task) => task.isDone).length;
-  
-  List<Task> get task => tasks;
+  int get finishedTasks => tasks.where((t) => t.isDone).length;
+  int get unfinishedTasks => tasks.where((t) => !t.isDone).length;
 
-  void addTask(String title) {
-    tasks.add(Task(name: title));
+  Future<void> loadTasksFromDB() async {
+    final dbTasks = await DbHelper().getTasks();
+    tasks = dbTasks.map((map) => Task.fromMap(map)).toList();
     notifyListeners();
   }
 
-  void updateTask(Task task, String newText) {
-    task.name = newText;
+  Future<void> addTask(String name) async {
+    int id = await DbHelper().insertTask(name);
+    tasks.add(Task(id: id, name: name));
     notifyListeners();
   }
 
-  void removeTask(Task task) {
+  Future<void> updateTask(Task task, String newName) async {
+    task.name = newName;
+    await DbHelper().updateTask(task.id!, task.name, task.isDone ? 1 : 0);
+    notifyListeners();
+  }
+
+  Future<void> toggleTask(Task task) async {
+    task.isDone = !task.isDone;
+    await DbHelper().updateTask(task.id!, task.name, task.isDone ? 1 : 0);
+    notifyListeners();
+  }
+
+  Future<void> removeTask(Task task) async {
+    await DbHelper().deleteTask(task.id!);
     tasks.remove(task);
     notifyListeners();
   }
-
-  void toggleTask(Task task) {
-    task.isDone = !task.isDone;
-    notifyListeners();
-
-  }
-
 }
